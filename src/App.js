68db390b8de2d67c2612ae495ff46765aeb6abce
Clement Sung <clement.sung7@gmail.com>
2021-05-17 12:39:57 -0400

    updating App.js

--- src/App.js
+++ src/App.js
@@ -3,21 +3,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>deploying with netlify</h1>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reprehenderit, ut nihil alias voluptates nemo laudantium labore provident, tenetur error dolorem atque ipsam iste at delectus possimus quasi voluptatum ipsa illo omnis voluptas aperiam natus fugiat adipisci? Assumenda corporis labore dicta deleniti itaque quaerat! Debitis atque odit, sed perferendis reprehenderit eveniet asperiores, aut laboriosam expedita esse aspernatur, vel quo dolorum veniam molestiae ad! Quos amet error exercitationem iusto odit. Autem eum saepe doloremque ex suscipit cumque, quaerat dolorum et, modi ut dignissimos quidem beatae iste, dolore libero minima officiis esse praesentium? Perspiciatis enim maiores dicta alias, numquam cum! Nesciunt ullam quasi hic officiis, quisquam magni non ipsum. Esse itaque dolorem corporis sequi debitis praesentium accusamus veritatis architecto? Inventore voluptas sit est voluptate consectetur! Similique animi totam placeat quas assumenda atque odio perferendis aspernatur, recusandae, sint quod dicta a numquam deleniti quos natus officiis in accusamus error, sit ratione. Quisquam explicabo impedit suscipit. Inventore in cupiditate obcaecati quas ipsam officiis, aut possimus asperiores voluptas minima vitae consequuntur? Quos quae suscipit atque obcaecati! Nam odit voluptates molestias ad optio iste dolorum molestiae, eaque voluptatem harum tempora iure velit vero excepturi quod eius aliquid aperiam dolores nulla voluptatibus! Repellat fugit exercitationem repudiandae eaque. Adipisci!</p>
     </div>
   );
 }
